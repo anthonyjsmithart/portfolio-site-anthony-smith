457bf4b198437ee51ce36ba1444041dde25e0c5a
anthonyjsmithart <anthonyjsmithart@gmail.com>
2022-08-01 21:19:55 -0400

    Add hamburger menu function

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-console.log("Hello, welcome to my portfolio site.")
+console.log ("Hello, welcome to my portfolio site.")
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+// hamburger menu
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
